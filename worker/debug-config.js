f7c40018e76ae5b29ad3f67e7078393bb1ce2ef6
vlaven96 <vladimir.ventaniuc@bp.com>
2024-12-21 16:42:09 +0200

    enable debug mode

---
+++ worker/debug-config.js
@@ -0,0 +1,12 @@
+process.env.ACCOUNT_USERNAME = 'tinselfu';
+process.env.PASSWORD = 'joy28#019';
+process.env.CUPID_TOKEN = '5cb75ec7721e3ed209fa22fc55480edf';
+process.env.MODEL_NAME = 'Leah';
+process.env.PROXY_HOST = '144.208.94.37';
+process.env.PROXY_PORT = '44444';
+process.env.PROXY_USERNAME = '14a8d3191c605';
+process.env.PROXY_PASSWORD = 'b7138d21d8';
+process.env.TWOFA_SECRET = 'URY3WDFRGWWAPUXUA5QEZYSBNAE3EMUX';
+process.env.isHotBot = 'false';
+
+console.log("Debug configuration loaded.");
